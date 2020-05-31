ff95f6b2cd522007bbc1f153c3039c166d55b2a0
Evgeny Pustolenko <pustolenko@yandex.ru>
2020-05-31 21:44:19 +0300

    Update fork-run.js

--- fork-run.js
+++ fork-run.js
@@ -3,9 +3,10 @@ global.NETWORK = "WBN-MAIN"; //max 10 chars
 global.START_NETWORK_DATE = 1590866000000; //formula of it value is (new Date(2019, 8, 20, 17, 0, 0, 0))-0;
 global.CONSENSUS_PERIOD_TIME = 2000; //ms
 global.FORK_IP_LIST = [
-  {"ip":"188.170.194.168","port":50005},
-  {"ip":"194.67.221.153","port":50005},
-  {"ip":"10.42.0.1","port":50005},
+  {"ip":"185.26.121.248","port":50005},   // hostland
+  {"ip":"194.67.221.153","port":50005},   // ihor vasyliy
+  {"ip":"188.170.194.168","port":50005},  // modem router
+  {"ip":"10.42.0.1","port":50005},        // ubuntu router
   {"ip":"127.0.0.1","port":50005},
   {"ip":"127.0.0.1","port":50006},
   {"ip":"127.0.0.1","port":50007}];
