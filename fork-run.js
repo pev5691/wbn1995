81b499b4a21998c665333ec079a790a3d799d46a
pev5691 <pev5691@yandex.ru>
2020-08-20 23:24:02 +0300

    Брендирование кошелька под WBN

--- fork-run.js
+++ fork-run.js
@@ -5,7 +5,7 @@ global.CONSENSUS_PERIOD_TIME = 2000; //ms
 
 global.FORK_IP_LIST = [
 //    {"ip":"185.26.121.248","port":50005},   // hostland
-    {"ip":"194.67.221.153","port":50005},   // ihor vasyliy
+    {"ip":"194.67.221.153","port":50005},   // ihor totoha
 //    {"ip":"188.227.85.27","port":50005},    // Server Space
     {"ip":"194.147.78.188","port":50005}];  // Vdska 194.147.78.188
 
