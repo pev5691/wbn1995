afdffab5e007fc56f7a6987c6a5c72f10c916284
pev5691 <pev5691@yandex.ru>
2020-09-07 20:34:40 +0400

    34347 NET_WORK_MODE cryptonetwork 34374 HTTP_PORT_NUMBER admin node 34734 HTTP_HOSTING_PORT ServerMainMap 37434 JINN_PORT

--- Source/HTML/JS/wallet-web.js
+++ Source/HTML/JS/wallet-web.js
@@ -21,8 +21,8 @@ var ConnectedCount = 0;
 var NETWORK_NAME = "WBN-MAIN";
 var ServerMap = {};
 var ServerMainMap = {
-    "vdska.wellbeing.su":{"ip":"vdska.wellbeing.su", "port":34734, "Name":"vdska", "System":1},
     "totoha.wellbeing.su":{"ip":"totoha.wellbeing.su", "port":34734, "Name":"totoha", "System":1},
+    "vdska.wellbeing.su":{"ip":"vdska.wellbeing.su", "port":34734, "Name":"vdska", "System":1},
     };
 var ServerTestMap = {"127.0.0.1":{"ip":"127.0.0.1", "port":80, "Name":"LOCAL"},
     "dappsgate.com":{"ip":"dappsgate.com", "port":88, "Name":"SUPPORT1", "System":1}, };
