15488e4daa31db54f8d7dcd07b40942a7096fb23
pev5691 <pev5691@yandex.ru>
2020-09-07 18:45:06 +0400

    34734 -> 34374

--- Source/HTML/JS/wallet-web.js
+++ Source/HTML/JS/wallet-web.js
@@ -21,13 +21,13 @@ var ConnectedCount = 0;
 var NETWORK_NAME = "WBN-MAIN";
 var ServerMap = {};
 var ServerMainMap = {   // Порты те же самые, что и в fork-run.js переменной global.FORK_IP_LIST
-//    "127.0.0.1":{"ip":"127.0.0.1", "port":34734, "Name":"LOCAL"},
+//    "127.0.0.1":{"ip":"127.0.0.1", "port":34374, "Name":"LOCAL"},
 //    "127.0.0.1":{"ip":"127.0.0.1", "port":50501, "Name":"LOCAL"},
 //    "127.0.0.1":{"ip":"127.0.0.1", "port":50502, "Name":"LOCAL"},
-    "vdska.wellbeing.su":{"ip":"vdska.wellbeing.su", "port":34734, "Name":"vdska", "System":1},
+    "vdska.wellbeing.su":{"ip":"vdska.wellbeing.su", "port":34374, "Name":"vdska", "System":1},
 //    "wbn3.wellbeing.su":{"ip":"vdska.wellbeing.su", "port":50501, "Name":"wbn1", "System":1},
 //    "wbn4.wellbeing.su":{"ip":"vdska.wellbeing.su", "port":50502, "Name":"wbn2", "System":1},
-    "totoha.wellbeing.su":{"ip":"totoha.wellbeing.su", "port":34734, "Name":"totoha", "System":1},
+    "totoha.wellbeing.su":{"ip":"totoha.wellbeing.su", "port":34374, "Name":"totoha", "System":1},
 //    "wbn1.wellbeing.su":{"ip":"totoha.wellbeing.su", "port":50501, "Name":"wbn3", "System":1},
 //    "wbn2.wellbeing.su":{"ip":"totoha.wellbeing.su", "port":50502, "Name":"wbn4", "System":1},
     };
