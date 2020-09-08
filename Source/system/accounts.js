540c019f8ae6bd1fc583f7678f75df814aeb3dc9
pev5691 <pev5691@yandex.ru>
2020-09-08 15:32:36 +0400

    Изучение первичных эккаунтов

--- Source/system/accounts.js
+++ Source/system/accounts.js
@@ -247,7 +247,7 @@ class AccountApp extends require("./dapp")
         
         this.DBStateWriteInner({Num:8, PubKey:GetArrFromHex(ARR_PUB_KEY[0]), Value:{BlockNum:1, SumCOIN:0 * TOTAL_SUPPLY_WBN},
             Name:"Founder account"})
-        this.DBStateWriteInner({Num:10, PubKey:GetArrFromHex(ARR_PUB_KEY[2]), Value:{BlockNum:1, SumCOIN:0}, Name:" Developer account"})
+        this.DBStateWriteInner({Num:10, PubKey:GetArrFromHex(ARR_PUB_KEY[0]), Value:{BlockNum:1, SumCOIN:0}, Name:" Developer account"})
         for(var i = 11; i < BLOCK_PROCESSING_LENGTH2; i++)
             this.DBStateWriteInner({Num:i, PubKey:GetArrFromHex(ARR_PUB_KEY[i - 8]), Value:{BlockNum:1}, Name:"Investor account"})
         
