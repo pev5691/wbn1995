ca7fa25a5ef8155dd97c1aeeae0500951f414668
pev5691 <pev5691@yandex.ru>
2020-09-09 08:38:10 +0300

    Обдираем майнинг, как липку.

--- Source/system/accounts.js
+++ Source/system/accounts.js
@@ -245,12 +245,12 @@ class AccountApp extends require("./dapp")
         for(var i = 1; i < 8; i++)
             this.DBStateWriteInner({Num:i, PubKey:[], Value:{BlockNum:1}, Name:"/dev/null"})
         
-        this.DBStateWriteInner({Num:8, PubKey:GetArrFromHex(ARR_PUB_KEY[0]), Value:{BlockNum:1, SumCOIN:0 * TOTAL_SUPPLY_WBN}, Name:"Founder account"})
+        this.DBStateWriteInner({Num:8, PubKey:GetArrFromHex(ARR_PUB_KEY[0]), Value:{BlockNum:1, SumCOIN:0}, Name:" Founder account"})
         this.DBStateWriteInner({Num:9, PubKey:GetArrFromHex(ARR_PUB_KEY[1]), Value:{BlockNum:1, SumCOIN:0}, Name:" Developer account"})
-        this.DBStateWriteInner({Num:10, PubKey:GetArrFromHex(ARR_PUB_KEY[2]), Value:{BlockNum:1, SumCOIN:0}, Name:" Investor account 1"})
-        this.DBStateWriteInner({Num:11, PubKey:GetArrFromHex(ARR_PUB_KEY[3]), Value:{BlockNum:1, SumCOIN:0}, Name:" Investor account 2"})
+        this.DBStateWriteInner({Num:10, PubKey:GetArrFromHex(ARR_PUB_KEY[2]), Value:{BlockNum:1, SumCOIN:0}, Name:""})
+        this.DBStateWriteInner({Num:11, PubKey:GetArrFromHex(ARR_PUB_KEY[3]), Value:{BlockNum:1, SumCOIN:0}, Name:""})
             for(var i = 12; i < BLOCK_PROCESSING_LENGTH2; i++)
-        this.DBStateWriteInner({Num:i, PubKey:GetArrFromHex(ARR_PUB_KEY[i - 8]), Value:{BlockNum:1}, Name:"Investor account"})
+        this.DBStateWriteInner({Num:i, PubKey:GetArrFromHex(ARR_PUB_KEY[i - 8]), Value:{BlockNum:1}, Name:""})
         
         this.DBStateTX.Write({Num:0, BlockNum:0})
         
@@ -620,11 +620,31 @@ class AccountApp extends require("./dapp")
                 if(!ISZERO(CoinDevelop))
                 {
                     OperationNum++
+                    this.SendMoneyTR(Block, 0, 8, CoinDevelop, Block.BlockNum, 0xFFFF, "", "Founder account", 1, 0, OperationNum)
+                }
+                {
+                    OperationNum++
                     this.SendMoneyTR(Block, 0, 9, CoinDevelop, Block.BlockNum, 0xFFFF, "", "Developers support", 1, 0, OperationNum)
                 }
                 {
                     OperationNum++
-                    this.SendMoneyTR(Block, 0, 10, CoinDevelop, Block.BlockNum, 0xFFFF, "", "Incoming P&R found", 1, 0, OperationNum)
+                    this.SendMoneyTR(Block, 0, 10, CoinDevelop, Block.BlockNum, 0xFFFF, "", "Incoming P&R fond", 1, 0, OperationNum)
+                }
+                {
+                    OperationNum++
+                    this.SendMoneyTR(Block, 0, 11, CoinDevelop, Block.BlockNum, 0xFFFF, "", "Fond no 1", 1, 0, OperationNum)
+                }
+                {
+                    OperationNum++
+                    this.SendMoneyTR(Block, 0, 12, CoinDevelop, Block.BlockNum, 0xFFFF, "", "Fond № 2", 1, 0, OperationNum)
+                }
+                {
+                    OperationNum++
+                    this.SendMoneyTR(Block, 0, 13, CoinDevelop, Block.BlockNum, 0xFFFF, "", "Fond bounty", 1, 0, OperationNum)
+                }
+                {
+                    OperationNum++
+                    this.SendMoneyTR(Block, 0, 14, CoinDevelop, Block.BlockNum, 0xFFFF, "", "Fond eshe kakoi", 1, 0, OperationNum)
                 }
             }
         }
